2907d0d4ba4339a19ab990c874d9775b8fedb0dc
Nurgul Amat <nurgulamat212@gmail.com>
2021-12-29 21:36:43 -0600

    used template literal

--- js/script.js
+++ js/script.js
@@ -114,7 +114,7 @@ function getBackgroundColor() {
   let g = Math.floor(Math.random() * 256);
   let b = Math.floor(Math.random() * 256);
 
-  let rgbColor = "rgb(" + r + "," + g + "," + b + ")";
+  let rgbColor = `rgb(${r}, ${g}, ${b})`;
   return (rgbColor);
 }
 
